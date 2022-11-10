94d328c24b55455873b555d24abbb1ee7cb1c22c
black_voodoo <developmest@gmail.com>
2022-11-10 10:42:04 +0300

    added programm on c#

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿// Control-work #1
+// Функция печатающая одномерный массив
+void PrintArray(string [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+}
+
+string [] array = {"hello", "2", "world", "^_^"};
+PrintArray(array);
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3){count ++;}
+}
+
+string [] resArray = new string [count];
+int j = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        resArray[j] = array[i];
+        j++;
+    }
+}
+
+Console.WriteLine();
+PrintArray(resArray);
